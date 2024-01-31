54627b50a8c1d849f6b272a446bd1022de1d1c5a
Abdul Joseph <abduljseph@gmail.com>
2024-01-30 21:21:19 -0600

    Completed section2.2 JsVariabklesAndPrimitives

--- Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -1,12 +1,16 @@
 // create a constant variable (const)
-const myName =
+const myName = "Abdul";
 // create a variable that can be reassigned (let)
-
+let age = 30;
 // create three variables and assign them values of different data types (=)
+let myString = "This is my string";
+let myNumber = "25";
+let myBoolean = "true";
 
 // print the types of two variables that reference two different data types (typeof)
 // TIP: console.log() prints a value in the console/terminal
-
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
 // change the value referenced by a variable (dynamic typing)
 
 // print the type of the variable you just changed (typeof)
@@ -15,4 +19,4 @@ const myName =
 // Which variables need to be created?
 // What type of data needs to go in each variable?
 
-console.log("Hello, my name is " + {add a name} + ", and I am learning " + {add a language} + ". I have been practicing for only " +  {add a number} + " weeks, so my status as a master is: " + {true or false} + ".");
+// console.log("Hello, my name is " + {add a name} + ", and I am learning " + {add a language} + ". I have been practicing for only " +  {add a number} + " weeks, so my status as a master is: " + {true or false} + ".");
---
+++ Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,53 @@
+// create a variable called "name" that references a string
+let name ="Abdul";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Death Around the Corner";
+
+// create a variable called "wage" that references a number
+let wage = 6;
+
+// create a variable called "age" that references a number
+let age = 39;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = "false";
+// create a variable called "satisfied" that references a boolean
+let satisfied = "true ";
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "a";
+console(typeof favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "a"
+console.log(typeof favoriteSong);
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+
+// Happy Birthday!
+// Reassign "age" to a new value
+age =42'
+
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 500;
+
+// Your raise changed your job satisfaction.
+satisfied = "true";
+// Assign "satisfied" to true
+
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("My name is + Abdul")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
