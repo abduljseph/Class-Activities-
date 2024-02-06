511dff7bdc4cb9086b4f41d62d48eb8cdfd83982
Abdul Joseph <abduljseph@gmail.com>
2024-02-05 21:36:58 -0600

    3.1.3-ReturnsAndShortCircuiting.js Solutions

--- Section03/3.1.3-ReturnsAndShortCircuiting.js
+++ Section03/3.1.3-ReturnsAndShortCircuiting.js
@@ -1,4 +1,5 @@
 // ? what does the function below return?
+// it returns undefined
 function myNumberFunction() {
   let num = 2 ** 2;
   num = num + 3;
@@ -8,17 +9,24 @@ function myNumberFunction() {
 }
 
 // change the function above to return a value
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+  return num;
+}
 
 // Short Circuiting
 // ! the console.log will not print
 function shortCircuitFunction() {
   return "Zap! - Short Circuited!";
-  console.log(
-    "This will never print because it is after the return statement."
-  );
+  console.log("This will never print because it is after the return statement.");
 }
 
 // ? will the following function short circuit?
+// no there is no return statement
 let height = 50;
 function checkCircusRides() {
   if (height < 48) {
@@ -31,3 +39,14 @@ function checkCircusRides() {
 }
 
 // change the function above to use short circuiting
+
+let height = 50;
+function checkCircusRides() {
+  if (height < 48) {
+    return("Take a ride on the carousel!");
+  }
+  if (height < 60) {
+    return("Try the bumper cars!");
+  }
+  return("Enjoy the roller coaster!");
+}
