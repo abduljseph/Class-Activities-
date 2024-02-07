6f8614ec704492316a641e661ed5ec916ada3532
Abdul Joseph <abduljseph@gmail.com>
2024-02-06 20:24:12 -0600

    3.1.4-Parameters.js Solutions

--- Section03/3.1.4-Parameters.js
+++ Section03/3.1.4-Parameters.js
@@ -1,20 +1,36 @@
 // Named Parameters
 // write a function that takes two named parameters:
+function functionWithTwoParams(param1,param2) {
+    console.log(param1, param2);
+    return param1 + param2;
+}
 // print each named parameter,
 // then return the parameters added together
+// return param1 + param2;
 
 // invoke the function and pass in two numbers
-
+console.log(functionWithTwoParams(2,3));
 // invoke the function and pass in more than two numbers
+console.log(functionWithTwoParams(2, 3, 4));
 
 // invoke the function and pass in only one number
-
+console.log (functionWithTwoParams(2));
 // change the function to set default values for the parameters
-
+function functionWithTwoParams(param1 = 10,param2 = 20) {
+    console.log(param1, param2);
+    return param1 + param2;
+}
 // again, invoke the function and pass in only one number
-
+console.log (functionWithTwoParams(2));
 // Rest Operator
 // add a rest operator to the function's parameters
 // add a line to the function's body to print the value of the rest operator
 
+function functionWithTwoParams(param1,param2, ...param3) {
+    console.log(param1, param2);
+    console.log(param3);
+    return param1 + param2;
+}
+
 // again, invoke the function and pass in more than two numbers
+console.log (functionWithTwoParams(2, 10, 30, 40));
