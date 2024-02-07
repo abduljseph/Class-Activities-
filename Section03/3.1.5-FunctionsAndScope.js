68eab6cf29c3f07584138c6397bc7a1e0c5f17da
Abdul Joseph <abduljseph@gmail.com>
2024-02-06 21:26:23 -0600

    3.1.5-FunctionsAndScope.js Solutions

--- Section03/3.1.5-FunctionsAndScope.js
+++ Section03/3.1.5-FunctionsAndScope.js
@@ -1,18 +1,24 @@
 // wrap the code below in a function called "addNumbersFrom1To100" and return the total
 // ! do not use console.log inside your function
 
+function addNumbersFrom1To100 () {
 let total = 0;
 for (let i = 1; i <= 100; i++) {
   total += i;
 }
+return total;
+}
+
 
 // invoke the function and console.log its value
-
+console.log(addNumbersFrom1To100());
 // try to print the variable "total" outside the function
 // ? what do you expect ?
-
+console.log(total);
 // examine the code below,
+
 // ? which console.log statement will not print when you invoke the parentAndChildScope function ?
+// the 2nd function
 function childScope(param) {
   const childVariable = "I am in the child's scope";
   console.log("Child Scope, Parent Variable:", param);
